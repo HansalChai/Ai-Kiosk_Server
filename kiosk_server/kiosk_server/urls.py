7b42b56f774605f3e11bb17ef4225704bd4e5d1d
cho1n <cjaewon0217@naver.com>
2024-05-12 17:07:31 +0900

    Feat: 초기 views, url 설정

--- kiosk_server/kiosk_server/urls.py
+++ kiosk_server/kiosk_server/urls.py
@@ -18,9 +18,11 @@ Including another URLconf
 from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
+from . import views
 
 
 urlpatterns = [
+    path('', views.home, name='home'),
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls'))
 ]
