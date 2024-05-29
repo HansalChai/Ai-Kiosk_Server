3d6d7cb1aa36d0b133d2fe3c9385a94838054351
cho1n <cjaewon0217@naver.com>
2024-05-29 15:09:19 +0900

    Feat: 회원 Patch 완료

--- kiosk_server/accounts/views.py
+++ kiosk_server/accounts/views.py
@@ -18,3 +18,20 @@ class CustomUserDetailView(APIView):
         user.IsDeleted = True
         user.save()
         return Response({'message': '사용자 프로필이 삭제되었습니다.'}, status=status.HTTP_200_OK)
+
+    def patch(self, request):
+        user = request.user
+        name = request.data.get('name')
+        password = request.data.get('password')
+        
+        if name:
+            user.name = name
+        if password:
+            user.set_password(password)  # 비밀번호를 해시하여 저장합니다.
+
+        user.save()
+
+        # 사용자 정보를 다시 불러와서 직렬화합니다.
+        serializer = CustomUserSerializer(user)
+        
+        return Response({'message': '프로필 수정 완료.', 'data': serializer.data}, status=status.HTTP_200_OK)
