b2b13ab1019dd83453847b2d8eddc9b001c815a5
jaewoo <wjdwodn0722@naver.com>
2024-06-03 00:24:11 +0900

    Feat: 메뉴조회 및 생성 기능 추가

--- kiosk_server/order/urls.py
+++ kiosk_server/order/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import CategoryListView, CategoryDetailView, CategoryUpdateView
+from .views import CategoryListView, CategoryDetailView, CategoryUpdateView, MenuListCreateView
 
 urlpatterns = [
     path('categories/', CategoryListView.as_view(), name='categories'),
     path('categories/<int:category_id>/', CategoryDetailView.as_view(), name='category-detail'),
     path('categories/<int:category_id>/update/', CategoryUpdateView.as_view(), name='category-update'),
+    path('categories/<int:category_id>/menus/', MenuListCreateView.as_view(), name='menu-list-create'),
+
 ]
