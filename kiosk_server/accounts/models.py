8a9ea6852e50bdb653a8d93977424e8c118e9ac5
cho1n <cjaewon0217@naver.com>
2024-06-03 21:17:28 +0900

    Fix: accounts 쪽 Models field명 수정

--- kiosk_server/accounts/models.py
+++ kiosk_server/accounts/models.py
@@ -2,21 +2,21 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class CustomUser(AbstractUser):
-    IsDeleted = models.BooleanField(default=False)
+    is_deleted = models.BooleanField(default=False)
     name = models.CharField(max_length=30)
-    createdAt = models.DateTimeField(auto_now_add=True)
-    updatedAt = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
         db_table = 'Owners'
         
 class Membership(models.Model):
-    ID = models.AutoField(primary_key=True, null=False)
+    id = models.AutoField(primary_key=True, null=False)
     phone_number = models.CharField(max_length=15, null=False, unique=True)
     total_point = models.IntegerField(null=False, default=0)
-    IsDeleted = models.BooleanField(default=False)
-    createdAt = models.DateTimeField(auto_now_add=True)
-    updatedAt = models.DateTimeField(auto_now=True)
+    is_deleted = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
         db_table = 'Membership'
