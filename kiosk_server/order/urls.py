c53bf2c62e564822f77c11db47faf12dc9f256d7
sumervilla99 <fkdmsgh828@dgu.ac.kr>
2024-06-03 19:13:42 +0900

    카테고리 url 수정, 옵션 url 추가

--- kiosk_server/order/urls.py
+++ kiosk_server/order/urls.py
@@ -1,10 +1,18 @@
 from django.urls import path
-from .views import CategoryListView, CategoryDetailView, CategoryUpdateView, MenuListCreateView, MenuDetailView
+from .views import CategoryListAddView, CategoryDeleteView, CategoryUpdateView, OptionsListAddView, OptionUpdateView, OptionDeleteView, MenuListCreateView, MenuDetailView
 
 urlpatterns = [
-    path('categories/', CategoryListView.as_view(), name='categories'),
-    path('categories/<int:category_id>/', CategoryDetailView.as_view(), name='category-detail'),
-    path('categories/<int:category_id>/update/', CategoryUpdateView.as_view(), name='category-update'),
+    # 카테고리 관련 API url
+    path('categories/', CategoryListAddView.as_view(), name='category-list-add'),
+    path('categories/<int:category_id>/', CategoryUpdateView.as_view(), name='category-update'),
+    path('categories/<int:category_id>/delete/', CategoryDeleteView.as_view(), name='category-delete'),
+    
+    # 옵션 관련 API url
+    path('categories/<int:category_id>/options/', OptionsListAddView.as_view(), name='option-list-add'),
+    path('categories/<int:category_id>/options/<int:option_id>/', OptionUpdateView.as_view(), name='option-update'),
+    path('categories/<int:category_id>/options/<int:option_id>/delete/', OptionDeleteView.as_view(), name='option-delete'),
+    
+    # 메뉴 관련 API url
     path('categories/<int:category_id>/menus/', MenuListCreateView.as_view(), name='menu-list-create'),
     path('categories/<int:category_id>/menus/<int:pk>/', MenuDetailView.as_view(), name='menu-detail'),
 
