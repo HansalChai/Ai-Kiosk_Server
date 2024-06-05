99958fee55c1ab2ba06ac0c7c8dce94732aa2079
jaewoo <wjdwodn0722@naver.com>
2024-06-05 16:16:01 +0900

    Fix: 주문내역 생성시 order_amount 올리도록 수정

--- kiosk_server/order/urls.py
+++ kiosk_server/order/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import CategoryListAddView, CategoryUpdateView, CategoryDeleteView, OptionsListAddView, OptionUpdateView, OptionDeleteView, MenuListCreateView, MenuDetailView, OrderAmountView, OrderCreateView
+from .views import CategoryListAddView, CategoryUpdateView, CategoryDeleteView, OptionsListAddView, OptionUpdateView, OptionDeleteView, MenuListCreateView, MenuDetailView, OrderCreateView
 
 urlpatterns = [
 
@@ -17,6 +17,6 @@ urlpatterns = [
     # 메뉴, 주문 관련 API path
     path('categories/<int:category_id>/menus/', MenuListCreateView.as_view(), name='menu-list-create'),
     path('categories/<int:category_id>/menus/<int:pk>/', MenuDetailView.as_view(), name='menu-detail'),
-    path('order/order_amount/', OrderAmountView.as_view(), name='order-amount'),
+    # path('order/order_amount/', OrderAmountView.as_view(), name='order-amount'),
     path('orders/', OrderCreateView.as_view(), name='order-create'),
 ]
